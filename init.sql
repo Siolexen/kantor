faac0f710fcfecefe6d113629b25d3f352f36d66
prusek <pawell.32@o2.pl>
2017-11-15 21:22:34 +0100

    Added wallets table

--- init.sql
+++ init.sql
@@ -11,6 +11,16 @@ CREATE TABLE TransactionHistory (
 	Id int NOT NULL AUTO_INCREMENT,
 	userId int NOT NULL,
 	currency varchar(255) NOT NULL,
-	price decimal NOT NULL,
+	price decimal (20, 2) NOT NULL,
+	PRIMARY KEY (Id)
+);
+CREATE TABLE Wallets (
+	Id int NOT NULL AUTO_INCREMENT,
+	userId int NOT NULL,
+	pln decimal (20, 2) NOT NULL,
+	eur decimal (20, 2) NOT NULL,
+	usd decimal (20, 2) NOT NULL,
+	gbp decimal (20, 2) NOT NULL,
+	chf decimal (20, 2) NOT NULL,
 	PRIMARY KEY (Id)
 );
