3b1ec31b837f3f110fa061ec06b0c81d89fcedff
prusek <pawell.32@o2.pl>
2017-11-23 19:48:15 +0100

    Changed variables of endsWith function

--- core/Route.php
+++ core/Route.php
@@ -3,8 +3,8 @@ class Route
 {
     private static $isValid = false;
 
-    private static function endsWith($haystack, $needle) {
-        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+    private static function endsWith($uri, $route) {
+        return $route === "" || (($temp = strlen($uri) - strlen($route)) >= 0 && strpos($uri, $route, $temp) !== false);
     }
 
     public static function set($route, $function) {
